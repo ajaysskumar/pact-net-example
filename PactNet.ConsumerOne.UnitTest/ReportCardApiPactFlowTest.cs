1ac7db130acef09c6b6995c2de2a9ef02156dcc5
Ajay kumar <ajay.a338@gmail.com>
2024-06-30 18:00:26 +0530

    Feature to include integration of pact flow (#2)
    [Feature]: Add code to integrate with pact flow

---
+++ PactNet.ConsumerOne.UnitTest/ReportCardApiPactFlowTest.cs
@@ -0,0 +1,108 @@
+using System.Net;
+using NUnit.Framework;
+using PactNet.ConsumerOne.HttpClients;
+using Xunit;
+using Assert = NUnit.Framework.Assert;
+using PactBrokerUtiliy = PactNet.ConsumerOne.UnitTest.Utilities.PactBrokerUtiliy;
+
+namespace PactNet.ConsumerOne.UnitTest
+{
+    public class ReportCardApiPactFlowTest
+    {
+        private readonly IPactBuilderV3 _pactBuilder;
+        private const string ValidStudentId = "067a50c5-0b23-485e-b018-17c66b2422ff";
+        
+        public ReportCardApiPactFlowTest()
+        {
+            var pactDir =
+                $"{Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.Parent?.FullName}{Path.DirectorySeparatorChar}pacts";
+
+            Console.WriteLine($"PACT_DIR: {pactDir}");
+            // or specify custom log and pact directories
+            var pact = Pact.V3("ConsumerOne", "Student API", new PactConfig
+            {
+                PactDir = pactDir,
+                LogLevel = PactLogLevel.Debug
+            });
+
+            // Initialize Rust backend
+            _pactBuilder = pact.WithHttpInteractions();
+        }
+
+        [Fact]
+        public async Task Get_Student_When_The_Student_With_Id_1_Exists()
+        {
+            // Arrange
+            _pactBuilder
+                .UponReceiving("A GET request to retrieve the student")
+                .Given($"There is student with id {ValidStudentId}")
+                .WithRequest(HttpMethod.Get, $"/students/{ValidStudentId}")
+                .WithHeader("Accept", "application/json")
+                .WillRespond()
+                .WithStatus(HttpStatusCode.OK)
+                .WithHeader("Content-Type", "application/json; charset=utf-8")
+                .WithJsonBody(new
+                {
+                    firstName = "Raju",
+                    lastName = "Rastogi",
+                    address = "Delhi",
+                    id = ValidStudentId
+                });
+
+            await _pactBuilder.VerifyAsync(async ctx =>
+            {
+                // Act
+                var client = new StudentClient(ctx.MockServerUri);
+                var studentDto = await client.GetStudentById(ValidStudentId);
+
+                // Assert
+                Assert.That(studentDto.Id, Is.EqualTo(ValidStudentId));
+            });
+            
+            await UploadPactContract();
+        }
+
+        [Fact]
+        public async Task Get_Student_When_The_StudentId_Is_Invalid()
+        {
+            // Arrange
+            _pactBuilder
+                .UponReceiving("A GET request to retrieve the student with invalid student id")
+                .Given("There is student is at least one valid student present")
+                .WithRequest(HttpMethod.Get, "/students/some-invalid-id")
+                .WithHeader("Accept", "application/json")
+                .WillRespond()
+                .WithStatus(HttpStatusCode.NoContent);
+
+            _pactBuilder.Verify(ctx =>
+            {
+                // Act
+                var client = new StudentClient(ctx.MockServerUri);
+                Assert.ThrowsAsync<Exception>(async () => await client.GetStudentById("some-invalid-id"));
+            });
+            
+            await UploadPactContract();
+        }
+        
+        private static async Task UploadPactContract()
+        {
+            var pactPath = Path.Combine("..",
+                "..",
+                "..",
+                "..",
+                "pacts",
+                "ConsumerOne-Student API.json");
+
+            var contractJson = File.ReadAllText(pactPath);
+            var pactFlowBaseUri = Environment.GetEnvironmentVariable("PACT_FLOW_BASE_URL"); // For testing purposes, you may even hardcode this value
+            var pactFlowToken = Environment.GetEnvironmentVariable("PACT_FLOW_TOKEN"); // For testing purposes, you may even hardcode this value
+            var statusCode = await PactBrokerUtiliy.PublishPactContract(
+                pactFlowBaseUri,
+                "ConsumerOne",
+                "Student API",
+                contractJson,
+                pactFlowToken,
+                Guid.NewGuid().ToString());
+        }
+    }
+}
--- PactNet.Provider.UnitTest/StudentControllerTest.cs
+++ PactNet.Provider.UnitTest/StudentControllerTest.cs
@@ -51,6 +51,39 @@ namespace PactNet.Provider.UnitTest
                 .WithProviderStateUrl(new Uri(_fixture.ServerUri, "/provider-states"))
                 .Verify();
         }
+        
+        [Fact]
+        public void Ensure_StudentApi_Honours_Pact_With_ConsumerOne_Using_PactFlow()
+        {
+            // Arrange
+            var config = new PactVerifierConfig
+            {
+                Outputters = new List<IOutput>
+                {
+                    // NOTE: PactNet defaults to a ConsoleOutput, however
+                    // xUnit 2 does not capture the console output, so this
+                    // sample creates a custom xUnit outputter. You will
+                    // have to do the same in xUnit projects.
+                    new XunitOutput(_output),
+                },
+                LogLevel = PactLogLevel.Information
+            };
+
+            var pactFlowBaseUri = Environment.GetEnvironmentVariable("PACT_FLOW_BASE_URL"); // For testing purposes, you may even hardcode this value
+            var pactFlowToken = Environment.GetEnvironmentVariable("PACT_FLOW_TOKEN"); // For testing purposes, you may even hardcode this value
+
+            // Act // Assert
+            IPactVerifier pactVerifier = new PactVerifier(config);
+            pactVerifier
+                .ServiceProvider("Student API", _fixture.ServerUri)
+                .WithPactBrokerSource(new Uri(pactFlowBaseUri), configure =>
+                {
+                    configure.TokenAuthentication(pactFlowToken);
+                    configure.PublishResults(true, "1.0.0"); // Any version
+                })
+                .WithProviderStateUrl(new Uri(_fixture.ServerUri, "/provider-states"))
+                .Verify();
+        }
     }
 }
 
